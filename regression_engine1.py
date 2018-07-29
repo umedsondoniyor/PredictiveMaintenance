e68fc95fd08dbfcc687b813b6a05183c084c264e
Umedzhon Izbasarov <umedzhonizbasarov@gmail.com>
2018-07-29 20:13:07 +0300

    everything is done except readme

---
+++ regression_engine1.py
@@ -0,0 +1,329 @@
+import keras
+import keras.backend as K
+from keras.models import load_model
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+from sklearn import preprocessing
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Activation
+
+# Setting seed for reproducability
+np.random.seed(1234)
+PYTHONHASHSEED = 0
+# define path to save model [;, ]
+model_path = 'Output/regression_model.h5'
+
+# read training data # read
+train_df = pd.read_csv('data/train/train_FD001.txt', sep=" ", header=None,engine='python')
+train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
+train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+# read test data
+test_df = pd.read_csv('data/test/test_FD001.txt', sep=" ", header=None,engine='python')
+test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+
+
+# read ground truth data# read
+truth_df = pd.read_csv('data/RUL/RUL_FD001.txt', sep=" ", header=None,engine='python')
+truth_df.drop(truth_df.columns[[1]], axis=1, inplace=True)
+
+# Data Labeling - generate column RUL
+rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+train_df = train_df.merge(rul, on=['id'], how='left')
+train_df['RUL'] = train_df['max'] - train_df['cycle']
+train_df.drop('max', axis=1, inplace=True)
+train_df.head()
+
+
+# generate label columns for training data
+w1 = 30
+w0 = 15
+train_df['label1'] = np.where(train_df['RUL'] <= w1, 1, 0 )
+train_df['label2'] = train_df['label1']
+train_df.loc[train_df['RUL'] <= w0, 'label2'] = 2
+train_df.head()
+
+# MinMax normalization
+train_df['cycle_norm'] = train_df['cycle']
+cols_normalize = train_df.columns.difference(['id','cycle','RUL','label1','label2'])
+min_max_scaler = preprocessing.MinMaxScaler()
+norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]),
+                             columns=cols_normalize,
+                             index=train_df.index)
+join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
+train_df = join_df.reindex(columns = train_df.columns)
+train_df.head()
+
+train_dftrain_d  = train_df.sort_values(['id','cycle'])
+train_df.head()
+
+# Data Labeling - generate column RUL
+rul = pd.DataFrame(train_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+train_df = train_df.merge(rul, on=['id'], how='left')
+train_df['RUL'] = train_df['max'] - train_df['cycle']
+train_df.drop('max', axis=1, inplace=True)
+train_df.head()
+
+
+# generate column max for test data# genera
+rul = pd.DataFrame(test_df.groupby('id')['cycle'].max()).reset_index()
+rul.columns = ['id', 'max']
+truth_df.columns = ['more']
+truth_df['id'] = truth_df.index + 1
+truth_df['max'] = rul['max'] + truth_df['more']
+truth_df.drop('more', axis=1, inplace=True)
+
+# generate RUL for test data
+test_df = test_df.merge(truth_df, on=['id'], how='left')
+test_df['RUL'] = test_df['max'] - test_df['cycle']
+test_df.drop('max', axis=1, inplace=True)
+test_df.head()
+
+
+# generate label columns w0 and w1 for test data# genera
+test_df['label1'] = np.where(test_df['RUL'] <= w1, 1, 0 )
+test_df['label2'] = test_df['label1']
+test_df.loc[test_df['RUL'] <= w0, 'label2'] = 2
+test_df.head()
+
+
+# pick a large window size of 50 cycles
+sequence_length = 50
+
+# preparing data for visualizations
+# window of 50 cycles prior to a failure point for engine id 3
+engine_id3 = test_df[test_df['id'] == 3]
+engine_id3_50cycleWindow = engine_id3[engine_id3['RUL'] <= engine_id3['RUL'].min() + 50]
+cols1 = ['s1', 's2', 's3', 's4', 's5', 's6', 's7', 's8', 's9', 's10']
+engine_id3_50cycleWindow1 = engine_id3_50cycleWindow[cols1]
+cols2 = ['s11', 's12', 's13', 's14', 's15', 's16', 's17', 's18', 's19', 's20', 's21']
+engine_id3_50cycleWindow2 = engine_id3_50cycleWindow[cols2]
+
+
+
+# plotting sensor data for engine ID 3 prior to a failure point - sensors 1-10 # plotti
+ax1 = engine_id3_50cycleWindow1.plot(subplots=True, sharex=True, figsize=(20,20))
+
+
+# plotting sensor data for engine ID 3 prior to a failure point - sensors 11-21 # plotti
+ax2 = engine_id3_50cycleWindow2.plot(subplots=True, sharex=True, figsize=(20,20))
+
+
+# function to reshape features into (samples, time steps, features) # functi
+def gen_sequence(id_df, seq_length, seq_cols):
+    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
+    we need to drop those which are below the window-length. An alternative would be to pad sequences so that
+    we can use shorter ones """
+    # for one id I put all the rows in a single matrix
+    data_matrix = id_df[seq_cols].values
+    num_elements = data_matrix.shape[0]
+    # Iterate over two lists in parallel.
+    # For example id1 have 192 rows and sequence_length is equal to 50
+    # so zip iterate over two following list of numbers (0,112),(50,192)
+    # 0 50 -> from row 0 to row 50
+    # 1 51 -> from row 1 to row 51
+    # 2 52 -> from row 2 to row 52
+    # ...
+    # 111 191 -> from row 111 to 191
+    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+        yield data_matrix[start:stop, :]
+
+
+# pick the feature columns
+sensor_cols = ['s' + str(i) for i in range(1,22)]
+sequence_cols = ['setting1', 'setting2', 'setting3', 'cycle_norm']
+sequence_cols.extend(sensor_cols)
+
+# TODO for debug
+# val is a list of 192 - 50 = 142 bi-dimensional array (50 rows x 25 columns)
+val=list(gen_sequence(train_df[train_df['id']==1], sequence_length, sequence_cols))
+print(len(val))
+
+# generator for the sequences
+# transform each id of the train dataset in a sequence
+seq_gen = (list(gen_sequence(train_df[train_df['id']==id], sequence_length, sequence_cols))
+           for id in train_df['id'].unique())
+
+# generate sequences and convert to numpy array
+seq_array = np.concatenate(list(seq_gen)).astype(np.float32)
+print(seq_array.shape)
+
+
+
+def gen_labels(id_df, seq_length, label):
+    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
+    we need to drop those which are below the window-length. An alternative would be to pad sequences so that
+    we can use shorter ones """
+    # For one id I put all the labels in a single matrix.
+    # For example:
+    # [[1]
+    # [4]
+    # [1]
+    # [5]
+    # [9]
+    # ...
+    # [200]]
+    data_matrix = id_df[label].values
+    num_elements = data_matrix.shape[0]
+    # I have to remove the first seq_length labels
+    # because for one id the first sequence of seq_length size have as target
+    # the last label (the previus ones are discarded).
+    # All the next id's sequences will have associated step by step one label as target.
+    return data_matrix[seq_length:num_elements, :]
+
+# generate labels
+label_gen = [gen_labels(train_df[train_df['id']==id], sequence_length, ['label1'])
+             for id in train_df['id'].unique()]
+label_array = np.concatenate(label_gen).astype(np.float32)
+label_array.shape
+
+
+
+
+##################################
+# Modeling
+##################################
+
+def r2_keras(y_true, y_pred):
+    """Coefficient of Determination
+    """
+    SS_res =  K.sum(K.square( y_true - y_pred ))
+    SS_tot = K.sum(K.square( y_true - K.mean(y_true) ) )
+    return ( 1 - SS_res/(SS_tot + K.epsilon()) )
+
+# Next, we build a deep network.
+# The first layer is an LSTM layer with 100 units followed by another LSTM layer with 50 units.
+# Dropout is also applied after each LSTM layer to control overfitting.
+# Final layer is a Dense output layer with single unit and linear activation since this is a regression problem.
+nb_features = seq_array.shape[2]
+nb_out = label_array.shape[1]
+
+model = Sequential()
+model.add(LSTM(
+         input_shape=(sequence_length, nb_features),
+         units=100,
+         return_sequences=True))
+model.add(Dropout(0.2))
+model.add(LSTM(
+          units=50,
+          return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(units=nb_out))
+model.add(Activation("linear"))
+model.compile(loss='mean_squared_error', optimizer='rmsprop',metrics=['mae',r2_keras])
+
+print(model.summary())
+
+# fit the network
+history = model.fit(seq_array, label_array, epochs=50, batch_size=200, validation_split=0.05, verbose=2,
+          callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='min'),
+                       keras.callbacks.ModelCheckpoint(model_path,monitor='val_loss', save_best_only=True, mode='min', verbose=0)]
+          )
+
+# list all data in history
+print(history.history.keys())
+
+# summarize history for R^2
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['r2_keras'])
+plt.plot(history.history['val_r2_keras'])
+plt.title('model r^2')
+plt.ylabel('R^2')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("Output/model_r2.png")
+
+# summarize history for MAE
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['mean_absolute_error'])
+plt.plot(history.history['val_mean_absolute_error'])
+plt.title('model MAE')
+plt.ylabel('MAE')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("Output/model_mae.png")
+
+# summarize history for Loss
+fig_acc = plt.figure(figsize=(10, 10))
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'test'], loc='upper left')
+plt.show()
+fig_acc.savefig("Output/model_regression_loss.png")
+
+# training metrics
+scores = model.evaluate(seq_array, label_array, verbose=1, batch_size=200)
+print('\nMAE: {}'.format(scores[1]))
+print('\nR^2: {}'.format(scores[2]))
+
+y_pred = model.predict(seq_array,verbose=1, batch_size=200)
+y_true = label_array
+
+test_set = pd.DataFrame(y_pred)
+test_set.to_csv('Output/submit_train.csv', index = None)
+
+##################################
+# EVALUATE ON TEST DATA
+##################################
+
+# We pick the last sequence for each id in the test data
+seq_array_test_last = [test_df[test_df['id']==id][sequence_cols].values[-sequence_length:]
+                       for id in test_df['id'].unique() if len(test_df[test_df['id']==id]) >= sequence_length]
+
+seq_array_test_last = np.asarray(seq_array_test_last).astype(np.float32)
+print("seq_array_test_last")
+#print(seq_array_test_last)
+print(seq_array_test_last.shape)
+
+# Similarly, we pick the labels
+#print("y_mask")
+y_mask = [len(test_df[test_df['id']==id]) >= sequence_length for id in test_df['id'].unique()]
+label_array_test_last = test_df.groupby('id')['RUL'].nth(-1)[y_mask].values
+label_array_test_last = label_array_test_last.reshape(label_array_test_last.shape[0],1).astype(np.float32)
+print(label_array_test_last.shape)
+print("label_array_test_last")
+print(label_array_test_last)
+
+
+# if best iteration's model was saved then load and use it
+if os.path.isfile(model_path):
+    estimator = load_model(model_path,custom_objects={'r2_keras': r2_keras})
+
+    # test metrics
+    scores_test = estimator.evaluate(seq_array_test_last, label_array_test_last, verbose=2)
+    print('\nMAE: {}'.format(scores_test[1]))
+    print('\nR^2: {}'.format(scores_test[2]))
+
+    y_pred_test = estimator.predict(seq_array_test_last)
+    y_true_test = label_array_test_last
+
+    test_set = pd.DataFrame(y_pred_test)
+    test_set.to_csv('Output/submit_test.csv', index = None)
+
+    # Plot in blue color the predicted data and in green color the
+    # actual data to verify visually the accuracy of the model.
+    fig_verify = plt.figure(figsize=(100, 50))
+    plt.plot(y_pred_test, color="blue")
+    plt.plot(y_true_test, color="green")
+    plt.title('prediction')
+    plt.ylabel('value')
+    plt.xlabel('row')
+    plt.legend(['predicted', 'actual data'], loc='upper left')
+    plt.show()
+    fig_verify.savefig("Output/model_regression_verify.png")
